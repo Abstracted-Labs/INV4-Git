acc99f2753ecf16bc71b75fe2071199555b2be8f
S0raWasTaken <s0ra@duck.com>
2022-02-07 22:34:03 -0500

    feat: implement client + subxt

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,57 @@
-pub struct GitArchClient;
+#![allow(dead_code, clippy::too_many_arguments)]
+use sp_keyring::{sr25519::sr25519::Pair, AccountKeyring::Alice};
+use std::cmp::Ordering;
+use subxt::{subxt, DefaultConfig, DefaultExtra, PairSigner};
+
+use crate::primitives::{Error, GitRef, Settings};
+
+#[subxt(runtime_metadata_path = "invarch_metadata.scale")]
+pub mod invarch {}
+
+pub type Id = invarch::runtime_types::polkadot_parachain::primitives::Id;
+
+// TEMP: Workaround for parachain ID issue #315 on paritytech/subxt
+impl PartialEq for Id {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl Eq for Id {}
+
+impl PartialOrd for Id {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl Ord for Id {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+pub struct GitArchClient {
+    pub signer: PairSigner<DefaultConfig, DefaultExtra<DefaultConfig>, Pair>,
+}
+
+impl Default for GitArchClient {
+    fn default() -> Self {
+        Self {
+            signer: PairSigner::<DefaultConfig, DefaultExtra<DefaultConfig>, Pair>::new(
+                Alice.pair(),
+            ),
+        }
+    }
+}
+
+impl GitArchClient {
+    #[allow(unused_variables)]
+    async fn fetch(&self, settings: Settings, git_ref: GitRef) -> Result<(), Error> {
+        todo!()
+    }
+    #[allow(unused_variables)]
+    async fn push(&self, settings: Settings, local_ref: GitRef) -> Result<(), Error> {
+        todo!()
+    }
+}
