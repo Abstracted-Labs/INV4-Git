73b8053a8ba15249a98d4475939419f5950f8e61
S0raWasTaken <s0ra@duck.com>
2022-02-11 13:31:26 -0500

    fix: Manual debug implementation Displays the error information correctly, instead of `ErrorWrap(*actual error)` The actual printed error was being printed using the Debug trait, instead of the Display that was manually implemented.

--- src/error.rs
+++ src/error.rs
@@ -3,12 +3,17 @@ use std::{
     fmt::{Debug, Display, Formatter, Result},
 };
 
-#[derive(Debug)]
 pub struct ErrorWrap<T: Debug>(pub T);
+
+impl<T: Debug> Debug for ErrorWrap<T> {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "Internal git-remote-gitarch error: {:?}", &self.0)
+    }
+}
 
 impl<T: Debug> Display for ErrorWrap<T> {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "Internal git-remote-gitarch error: {:?}", &self.0)
+        write!(f, "{:?}", &self)
     }
 }
 
